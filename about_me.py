ea19ef47b82af03ec53a68195fddf94e088f58b1
Vibhor Kedia <vibhorkedia21@gmail.com>
2026-06-01 11:02:14 +0530

    Added streamlit about me app for deployment

---
+++ about_me.py
@@ -0,0 +1,24 @@
+import streamlit as st
+from PIL import Image
+
+st.title("Vibhor Kedia")
+
+st.header("Who am I:")
+st.write("Vibhor Kedia is a 12 year old boy who lives in Hyderabad 🌍.")
+
+st.markdown("---")
+
+st.header("My hobbies and interests:")
+st.write("""
+- Coding (prefers C++ but enjoys Python too)  
+- Robotics 🤖  
+- Abstract mathematics  
+- Cubing (~20s average on 3x3 Rubik’s Cube)  
+""")
+
+st.markdown("---")
+
+st.success("He is very proud of his skill in mathematics.")
+st.success("He is also very happy that he can solve a Rubik's cube in 20 seconds.")
+
+st.info("Fun fact: He learned cubing only by **YouTube** 🎥")
